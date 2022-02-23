e4f75abc88479060c7fa529ed65ce8a24c202fcb
Andy Leverenz <andy@justalever.com>
2022-02-22 18:08:50 -0600

    Add install script for brand new apps

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -1,3 +1,47 @@
-say "Build into app/assets/builds"
-empty_directory "app/assets/builds"
-keep_file "app/assets/builds"
+source_root = File.expand_path('../templates', __FILE__)
+
+say "Generate StaticController"
+generate "controller", "static index --skip-test-framework --skip-assets --skip-helper --skip-routes"
+
+# TODO: Copy content to index view
+
+say "Add default RailsUI routing and engine"
+content = <<-RUBY
+  if Rails.env.development? || Rails.env.test?
+    mount Railsui::Engine, at: "/railsui"
+  end
+
+  scope controller: :static do
+
+  end
+
+  root to: "static#index"
+RUBY
+
+insert_into_file "#{Rails.root}/config/routes.rb", "#{content}\n", after: "Rails.application.routes.draw do\n"
+
+if Rails.root.join("app/views/shared").exist?
+  say "app/views/shared already exists. Files can't be copied. Refer to the gem source for reference."
+else
+  say "Adding shared partials"
+  directory "#{__dir__}/shared", Rails.root.join("app/views/shared")
+end
+
+if (app_layout_path = Rails.root.join("app/views/layouts/application.html.erb")).exist?
+  say "Add meta tag partial"
+  insert_into_file(
+    app_layout_path.to_s,
+    %(\n    <%= render "shared/meta" %>),
+    before:/\s*<\/head>/
+  )
+
+  say "Add flash and nav partials"
+  insert_into_file(
+    app_layout_path.to_s,
+    %(
+    <%= render "shared/flash" %>
+    <%= render "shared/nav" %>
+    ),
+    after:"<body>"
+  )
+end
---
+++ lib/tasks/install.rake
@@ -0,0 +1,25 @@
+namespace :railsui do
+    desc "Install shared elements Rails UI CSS frameworks"
+    task :install do
+      system "#{RbConfig.ruby} ./bin/rails app:template LOCATION=#{File.expand_path("../install/install.rb",  __dir__)}"
+    end
+  namespace :framework do
+    namespace :install do
+
+      desc "Install Tailwind for Rails UI"
+      task tailwind: "framework:install" do
+        system "#{RbConfig.ruby} ./bin/rails app:template LOCATION=#{File.expand_path("../install/tailwind/install.rb",  __dir__)}"
+      end
+
+      desc "Install Bootstrap for Rails UI"
+      task bootstrap: "framework:install" do
+        system "#{RbConfig.ruby} ./bin/rails app:template LOCATION=#{File.expand_path("../install/bootstrap/install.rb",  __dir__)}"
+      end
+
+      desc "Install Bulma for Rails UI"
+      task bulma: "framework:install" do
+        system "#{RbConfig.ruby} ./bin/rails app:template LOCATION=#{File.expand_path("../install/bulma/install.rb",  __dir__)}"
+      end
+    end
+  end
+end
