e10dc585ebe147c99bbaf095b8d16ae99db37708
Andy Leverenz <andy@justalever.com>
2022-10-19 15:13:28 -0500

    Embed JS for demos

--- app/helpers/railsui/framework_helper.rb
+++ app/helpers/railsui/framework_helper.rb
@@ -3,7 +3,7 @@ module Railsui
 
    def framework_cdn_script
     if Railsui.config.css_framework ==  Railsui::Default::BOOTSTRAP
-    '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>'.html_safe
+    '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>'.html_safe
       elsif Railsui.config.css_framework == Railsui::Default::TAILWIND_CSS
         # TODO: Add Tailwind CSS / JS
       else
