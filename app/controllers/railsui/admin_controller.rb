2bbfd0c3f0b54dbb1cd89b8c9fd2a6170f6c5e27
Andy Leverenz <andy@justalever.com>
2022-10-24 15:45:08 -0500

    Overhaul configuration form

--- app/controllers/railsui/admin_controller.rb
+++ app/controllers/railsui/admin_controller.rb
@@ -2,6 +2,7 @@ require_dependency "railsui/application_controller"
 
 module Railsui
   class AdminController < ApplicationController
+    layout "railsui/fullwidth"
     def show
       @config = Railsui::Configuration.load!
     end
