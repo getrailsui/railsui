73c5bcaf939f18d0569b34af96803ad0ece59ae1
Andy Leverenz <andy@justalever.com>
2023-02-05 16:58:52 -0600

    Hound -> Tooltips

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -4,12 +4,16 @@ import tippy from "tippy.js"
 export default class extends Controller {
   static values = {
     content: String,
+    allowHtml: Boolean
   }
 
   connect() {
     let options = {}
     if (this.hasContentValue) {
       options["content"] = this.contentValue
+    }
+    if (this.hasAllowHtmlValue) {
+      options["allowHTML"] = this.allowHtmlValue
     }
     this.tippy = tippy(this.element, options)
   }
