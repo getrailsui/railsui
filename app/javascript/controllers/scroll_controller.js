0d7416cdb107c8d1ab28eab45c0225a73f38c22a
Andy Leverenz <andy@justalever.com>
2023-03-18 12:42:39 -0500

    Make routes nicer

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["scrollitem"]
+  static targets = ["scrollitem", "launcher"]
   connect() {
     if (this.hasScrollitemTarget) {
       let scrollpos = localStorage.getItem("scrollpos")
@@ -12,5 +12,20 @@ export default class extends Controller {
         localStorage.setItem("scrollpos", this.scrollitemTarget.scrollTop)
       }
     }
+
+    if (this.hasLauncherTarget) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= 800) {
+          this.launcherTarget.classList.remove('hidden')
+        } else {
+          this.launcherTarget.classList.add('hidden')
+        }
+      })
+    }
+  }
+
+  scrollToTop(event) {
+    event.preventDefault()
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
