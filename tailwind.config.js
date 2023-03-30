af0e65750c2d9913d444d26297bdf03a43aa3067
Andy Leverenz <andy@justalever.com>
2023-03-30 05:25:45 -0500

    Update deprecated BS classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
   ],
 }
