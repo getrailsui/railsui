614e898f034ebdc94170972120b4b175f55e289a
Andy Leverenz <andy@justalever.com>
2024-09-18 19:43:50 -0500

    Cleanup and refactoring

--- lib/railsui/install/application_mailer.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  helper Railsui::MailHelper
-
-  default from: email_address_with_name("#{Railsui.config.support_email}", "#{Railsui.config.application_name}")
-  layout "mailer"
-end
