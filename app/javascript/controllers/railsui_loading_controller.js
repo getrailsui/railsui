098b8e979b59ba016ebc717ff6cd1f60a139b6d4
Andy Leverenz <andy@justalever.com>
2024-09-27 12:29:23 -0500

    Improve loader and fix dashboard bugs

--- app/javascript/controllers/railsui_loading_controller.js
+++ app/javascript/controllers/railsui_loading_controller.js
@@ -1,11 +1,43 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["content"]
+  static targets = ["content", "saving"]
   static values = {
     label: { type: String, default: "Loading..." },
   }
+
+  initialize() {
+    const urlParams = new URLSearchParams(window.location.search)
+
+    if (urlParams.get("update") === "true") {
+      // Reload the page
+      setTimeout(() => {
+        this.removeURLParameter("update")
+        window.location.reload()
+      }, 3000)
+    }
+  }
+
   start() {
     this.contentTarget.innerHTML = this.labelValue
+
+    this.savingTarget.classList.add("config-loader--active")
+    document.body.classList.add("overflow-hidden")
+  }
+
+  connect() {
+    this.toggleLoader()
+  }
+
+  toggleLoader() {
+    // Remove loader
+    this.savingTarget.classList.remove("config-loader--active")
+    document.body.classList.remove("overflow-hidden")
+  }
+
+  removeURLParameter(param) {
+    const url = new URL(window.location.href)
+    url.searchParams.delete(param)
+    window.history.replaceState({}, "", url)
   }
 }
