8134bbfffedae3491a343871cbed6e6d8825d62f
Andy Leverenz <andy@justalever.com>
2025-05-31 15:38:13 -0500

    [hound] combobox and flash components

--- app/helpers/railsui/component_helper.rb
+++ app/helpers/railsui/component_helper.rb
@@ -1,5 +1,39 @@
 module Railsui
   module ComponentHelper
+    def rui(name, **locals, &block)
+      name = name.to_s
+
+      uses_items = locals.key?(:items)
+
+      # Figure out content only if block or content isn’t already given
+      content =
+        if block_given?
+          capture(&block)
+        elsif locals[:content].present?
+          locals[:content]
+        elsif !uses_items && locals[:label].present?
+          locals[:label]
+        end
+
+      segments = name.split("/")
+      folder = segments.first
+      partial = segments.length == 1 ? folder : segments.last
+
+      paths = [
+        "rui/components/#{folder}/#{partial}",
+        "components/#{folder}/#{partial}"
+      ]
+
+      found_path = paths.find do |path|
+        lookup_context.template_exists?(path, [], true)
+      end
+
+      raise "Rails UI component '#{name}' not found in: #{paths.join(', ')}" unless found_path
+
+      Rails.logger.debug "RailsUI: rendering #{found_path}" if Rails.env.development?
+      render partial: found_path, locals: locals.merge(content: content)
+    end
+
     # def rui(name, **locals, &block)
     #   name = name.to_s
     #   content = block_given? ? capture(&block) : (locals[:content] || locals[:label])
@@ -22,30 +56,30 @@ module Railsui
     #   Rails.logger.debug "RailsUI: rendering #{found_path}" if Rails.env.development?
     #   render partial: found_path, locals: locals.merge(content: content)
     # end
-    #
-    def rui(name, **locals, &block)
-      name = name.to_s
 
-      # Only assign content if block is given
-      content = block_given? ? capture(&block) : nil
+    # def rui(name, **locals, &block)
+    #   name = name.to_s
 
-      segments = name.split("/")
-      folder = segments.first
-      partial = segments.length == 1 ? folder : segments.last
+    #   # Only assign content if block is given
+    #   content = block_given? ? capture(&block) : nil
 
-      paths = [
-        "rui/components/#{folder}/#{partial}",
-        "components/#{folder}/#{partial}"
-      ]
+    #   segments = name.split("/")
+    #   folder = segments.first
+    #   partial = segments.length == 1 ? folder : segments.last
 
-      found_path = paths.find do |path|
-        lookup_context.template_exists?(path, [], true)
-      end
+    #   paths = [
+    #     "rui/components/#{folder}/#{partial}",
+    #     "components/#{folder}/#{partial}"
+    #   ]
 
-      raise "Rails UI component '#{name}' not found in: #{paths.join(', ')}" unless found_path
+    #   found_path = paths.find do |path|
+    #     lookup_context.template_exists?(path, [], true)
+    #   end
 
-      Rails.logger.debug "RailsUI: rendering #{found_path}" if Rails.env.development?
-      render partial: found_path, locals: locals.merge(content: content)
-    end
+    #   raise "Rails UI component '#{name}' not found in: #{paths.join(', ')}" unless found_path
+
+    #   Rails.logger.debug "RailsUI: rendering #{found_path}" if Rails.env.development?
+    #   render partial: found_path, locals: locals.merge(content: content)
+    # end
   end
 end
