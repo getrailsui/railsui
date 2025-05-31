fc760728605442ac4904bd4f731ca43d5fee6124
Andy Leverenz <andy@justalever.com>
2025-05-31 14:34:05 -0500

    [hound] combobox

--- app/helpers/railsui/component_helper.rb
+++ app/helpers/railsui/component_helper.rb
@@ -1,8 +1,33 @@
 module Railsui
   module ComponentHelper
+    # def rui(name, **locals, &block)
+    #   name = name.to_s
+    #   content = block_given? ? capture(&block) : (locals[:content] || locals[:label])
+
+    #   segments = name.split("/")
+    #   folder = segments.first
+    #   partial = segments.length == 1 ? folder : segments.last
+
+    #   paths = [
+    #     "rui/components/#{folder}/#{partial}",
+    #     "components/#{folder}/#{partial}"
+    #   ]
+
+    #   found_path = paths.find do |path|
+    #     lookup_context.template_exists?(path, [], true)
+    #   end
+
+    #   raise "Rails UI component '#{name}' not found in: #{paths.join(', ')}" unless found_path
+
+    #   Rails.logger.debug "RailsUI: rendering #{found_path}" if Rails.env.development?
+    #   render partial: found_path, locals: locals.merge(content: content)
+    # end
+    #
     def rui(name, **locals, &block)
       name = name.to_s
-      content = block_given? ? capture(&block) : (locals[:content] || locals[:label])
+
+      # Only assign content if block is given
+      content = block_given? ? capture(&block) : nil
 
       segments = name.split("/")
       folder = segments.first
@@ -17,9 +42,7 @@ module Railsui
         lookup_context.template_exists?(path, [], true)
       end
 
-      unless found_path
-        raise "Rails UI component '#{name}' not found in: #{paths.join(', ')}"
-      end
+      raise "Rails UI component '#{name}' not found in: #{paths.join(', ')}" unless found_path
 
       Rails.logger.debug "RailsUI: rendering #{found_path}" if Rails.env.development?
       render partial: found_path, locals: locals.merge(content: content)
